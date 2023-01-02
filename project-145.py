898b7af7743d25496ebf5869b3512fcc7cf7c7ef
rajveerjaiswal997 <88244284+rajveerjaiswal997@users.noreply.github.com>
2023-01-02 11:19:51 +0530

    Add files via upload

---
+++ project-145.py
@@ -0,0 +1,23 @@
+from tkinter import *
+root = Tk()
+
+root.title("drivinglicense")
+root.geometry("400x200")
+
+label_id=Label(root,text="id")
+label_name=Label(root,text="")
+label_dob=Label(root,text="dob")
+label_pin=Label(root,text="pin")
+label_address=Label(root,text="address")
+label_vehicle_type=Label(root,text="vehicle_type")
+
+a=10
+print(type(a))
+label_name=Label(root,"winnie the pooh")
+btn=Button(root,text="w")
+
+root.mainloop()
+
+
+ 
+ 
